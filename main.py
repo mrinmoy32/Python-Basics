43265ca2db7ddd253ec8c5d76302f7f0d8353236
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-04-28 16:40:41 +0530

    str and number

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ These lines are not executed as code.
 
 '''This also works
 for multiline comment'''
+
+# variables
+# string
+greet = "Hi, my name is Mrinmoy"
+print(greet)
+# number
+year = 2024
+temperature = 41.7 
+print(temperature, "deg C and", year)
+# List
+
+# Tuple
+# Dictionaries or Maps
