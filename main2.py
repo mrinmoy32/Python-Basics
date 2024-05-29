57d72daa3b0eb6f42510a93d63a46c74691e85bd
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-05-29 21:14:54 +0530

    Math Functions

--- main2.py
+++ main2.py
@@ -1,1 +1,14 @@
 # Continuation from main.py file
+# Math Functions
+import math #importing the math module
+pi = 3.14
+neg = - 32
+x,y,z = 1,2,-3
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(neg))
+print(pow(pi,2))
+print(math.sqrt(pi))
+print(max(x,y,z))
+print(min(x,y,z))
