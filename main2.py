66cdbf90bc1f9b4b634441dd669f3129b4b18a96
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-05-29 21:37:34 +0530

    String slicing using indexing[] -> [start:stop:step]

--- main2.py
+++ main2.py
@@ -12,3 +12,18 @@ print(pow(pi,2))
 print(math.sqrt(pi))
 print(max(x,y,z))
 print(min(x,y,z))
+
+# String slicing (create substring from a string)
+# using indexing[] -> [start:stop:step]
+name = "Virat Kohli"
+first_name = name[0:5] #Also print((name[:5])) would work
+last_name = name[6:11] # print((name[4:])) would work
+print(first_name)
+print(last_name)
+# lets use optional step (step is 1 by default)
+funky_name = name[::2]
+print(funky_name)
+reversed_name = name[::-1] # to reverse a string in python
+print(reversed_name)
+
+# using slice()
