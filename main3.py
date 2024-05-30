993b1ed143122307c70df3c682961fba79c83c53
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-05-30 21:55:25 +0530

    nested loops

--- main3.py
+++ main3.py
@@ -1,3 +1,11 @@
 # nested loops 
 #(the inner loop will finish all of it's iteration before finishing one iteration of the outer loop)
 
+rows = int(input("How many rows?: "))
+columns = int(input("How many columns?: "))
+symbol = input("Enter a symbol to use: ")
+
+for i in range(0,rows):
+    for j in range(0,columns):
+        print(symbol, end="") #end="" prevnts moving to new line
+    print() # print a new line
