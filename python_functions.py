2e60226daba27038a9b86ada9241526687d86e61
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-04 23:02:16 +0530

    return statement

--- python_functions.py
+++ python_functions.py
@@ -4,3 +4,9 @@ def hello(first_name, last_name, year):
     print("Hello" + " " + first_name + " " + last_name)
     print("Happy New Year!" + " " + str(year))
 hello("Mrinmoy", "Pal", 2024)
+
+#return statement (the value or object python sends back to the caller)
+def sum(num1, num2):
+    return num1 + num2
+print(sum(2,3))
+
