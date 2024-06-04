c293ab9b1ea25908318d199c210b266674bc9f10
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-04 22:42:01 +0530

    def hello()

--- python_functions.py
+++ python_functions.py
@@ -1,1 +1,6 @@
 # Function (A block of code which is executed only when it is called)
+
+def hello(first_name, last_name, year):
+    print("Hello" + " " + first_name + " " + last_name)
+    print("Happy New Year!" + " " + str(year))
+hello("Mrinmoy", "Pal", 2024)
