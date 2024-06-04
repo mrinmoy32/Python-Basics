11ad03b211006a8c765d62c29d6e6518e8bef859
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-04 23:08:52 +0530

    # keyword arguments

--- python_functions.py
+++ python_functions.py
@@ -10,3 +10,11 @@ def sum(num1, num2):
     return num1 + num2
 print(sum(2,3))
 
+# keyword arguments (arguments prceded by an identifier when we pass them to a function
+# The order of the arguments doesn't ,atter unlike the positional arguments
+# python knows the names of the arguments that our function receives)
+
+def greet(first_name, last_name, year):
+    print("Hello" + " " + first_name + " " + last_name)
+    print("Happy New Year!" + " " + str(year))
+greet(year=2024, last_name="Pal", first_name="Mrinmoy")
