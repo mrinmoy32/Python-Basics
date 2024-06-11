6df99a47e4902289d4c7e4aa97c7dfb5a3660830
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-11 13:37:54 +0530

    File detection

--- python_files.py
+++ python_files.py
@@ -1,1 +1,13 @@
 # Working with files in python
+#File detection
+import os
+path = "C:\\Users\\mrinm\\OneDrive\\Desktop\\file_for_python.txt" # need \\ in python (escape sequence for \)
+
+if os.path.exists(path):
+    print("That location exists")
+    if os.path.isfile(path):
+        print("It is a file")
+    elif os.path.isdir(path):
+        print("It is a directory")
+else:
+    print("That location doesn't exist")
