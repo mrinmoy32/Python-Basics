db16eaae6814d0690ebc2d7b6717d4d875cd5201
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-11 12:33:42 +0530

    #random module

--- python_functions.py
+++ python_functions.py
@@ -102,3 +102,16 @@ print("The amount is {:b}".format(amount)) #binary representation
 print("The amount is {:o}".format(amount)) #octal representation
 print("The amount is {:x}".format(amount)) #hex representation
 print("The amount is {:e}".format(amount)) #scientific representation
+
+#random module
+import random
+x = random.randint(1,6) # random num bet 1-6
+print(x)
+y = random.random() # random float
+print(y) 
+my_list = ['rock', 'paper', 'scissors']
+z = random.choice(my_list)
+print(z)
+cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+random.shuffle(cards) # shuffles the list
+print(cards)
