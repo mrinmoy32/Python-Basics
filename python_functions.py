acab6cbe27995ead9898d41a32673f2a5a4bf11a
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-11 12:51:29 +0530

    exception: else & finally

--- python_functions.py
+++ python_functions.py
@@ -124,10 +124,16 @@ try:
     numerator = int(input("Enter a number to divide: "))
     denominator = int(input("Enter a number to divide by: "))
     result = numerator/denominator
-    print(result)
-except ZeroDivisionError:
+except ZeroDivisionError as e:
+    print(e)
     print("You can't divide by zero!")
-except ValueError:
+except ValueError as e:
+    print(e)
     print("Please enter numbers only!")
-except Exception:
+except Exception as e:
+    print(e)
     print("Something went wrong!")
+else:
+    print(result) # else will excute if there is no exception
+finally:
+    print('This will always execute')
