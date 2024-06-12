606f595b020b72e2bb46652f8fd5e89a4dc34213
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-12 20:05:54 +0530

    # create a file

--- files_in_python/python_files.py
+++ files_in_python/python_files.py
@@ -7,7 +7,15 @@ if os.path.exists(path):
     print("That location exists")
     if os.path.isfile(path):
         print("It is a file")
-    elif os.path.isdir(path):
+    elif os.path.isdir(path): # we can also
         print("It is a directory")
 else:
     print("That location doesn't exist")
+
+# create a file
+
+try:
+    with open('example.txt', 'w') as file: # in place of 'example.txt' you can use any file path(C:\\Users\\user\\Desktop\\example.txt)
+        print('Empty file','example.txt', 'created successfully. Will write to this file later')
+except:
+    print('Error occurred while creating file')
