f7850594aaf63e189279f7144ac216c11a949c57
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-14 20:28:31 +0530

    modue in python

---
+++ modules_in_python/my_module.py
@@ -0,0 +1,8 @@
+# module (a module is a file containing python code. May contain functions, classes etc.
+# used with moduler programming, which is to sepearte a program into parts)
+
+def hello():
+    print('Hello from a module')
+
+def bye():
+    print('Bye from a module')
