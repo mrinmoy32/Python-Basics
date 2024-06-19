e399eb07eeac3cbefd200e6133a6d6c3be6ec156
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-19 21:15:00 +0530

    Assign function to a variable

--- higher_order_functions.py
+++ higher_order_functions.py
@@ -19,3 +19,17 @@
 print(happy := True) # True 
 # In the above example, the walrus operator is used to assign the value of input("Enter your name: ") to the variable name and use it in the if condition.
 # This makes the code more concise and readable.
+
+#-------------------Assign function to a variable-------------------
+# In Python, functions are first-class objects, which means they can be assigned to variables, passed as arguments to other functions, and returned from other functions.
+
+def greet(name):
+    return f"Hello, {name}"
+
+print(greet)
+greeting = greet # greeting gets the same memory location as greet to keep a reference to the function
+print(greeting("Alice")) # Hello, Alice
+
+#Another example
+out = print # Assign the print function to the variable out
+out("Hello, World!") # Hello, World!
