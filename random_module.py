95286366be24fc1fe9e0abc73d0a7c4d6091d0a4
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-06-21 20:15:56 +0530

    demonstrate the use of __name__ and __main__

---
+++ random_module.py
@@ -0,0 +1,7 @@
+# We will use this file to demonstrate the use of __name__ and __main__
+# Lets import this file in another file and see the output
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
